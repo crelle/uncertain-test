8d0cf2801618c938b7de3afb2af3c83ec35ec121
e <383362161@qq.com>
2021-02-20 10:40:20 +0800

    测试用例

--- src/main/java/crelle/test/java/lang/StringTest.java
+++ src/main/java/crelle/test/java/lang/StringTest.java
@@ -1,5 +1,7 @@
 package crelle.test.java.lang;
 
+import org.junit.Test;
+
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
 
@@ -11,26 +13,17 @@ import java.text.DecimalFormat;
  * @description:XX
  **/
 public class StringTest {
-    public static void main(String[] args) {
-        test5();
+
+    @Test
+    public void test() {
+        System.out.println("abc".replace("a", ""));
     }
 
-    private static void test(){
-        System.out.println("abc".replace("a",""));
-    }
 
-    /**
-     *@author:crelle
-     *@date:2021/1/26
-     *@title:test2
-     *@description:保留两位小数
-     *@params:[]
-     *@return:void
-     *@throw:
-     */
-    private static void test2(){
+    @Test
+    public void test2() {
         double f1 = 132;
-        System.out.println(String.format("%.2f",f1));
+        System.out.println(String.format("%.2f", f1));
 
         double f2 = 111231.5585;
 
@@ -38,25 +31,28 @@ public class StringTest {
         System.out.println(df.format(f2));
     }
 
-    private static void test3(){
-        System.out.println("1234567".substring(0,7));
+    @Test
+    public void test3() {
+        System.out.println("1234567".substring(0, 7));
     }
 
-    private static void test4(){
+    @Test
+    public void test4() {
         String qty = "100000";
         Double result = Double.valueOf(qty);
 //        BigDecimal decimal = new BigDecimal()
         Double newVal = Math.abs(result);
-        if(newVal<5000){
+        if (newVal < 5000) {
             System.out.println(newVal);
-        }else{
+        } else {
             System.out.println("大于5000！");
         }
     }
 
-    private static void test5(){
+    @Test
+    public void test5() {
         String str = "-123456789";
-        str =str.substring(1,str.length());
+        str = str.substring(1, str.length());
         System.out.println(str);
     }
 
